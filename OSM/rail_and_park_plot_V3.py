a67a063830db303f4ea6edc0f4ce9dcc48d92bca
yueying-teng <yt2495@columbia.edu>
2019-04-18 00:02:40 +0900

    clean

--- OSM/rail_and_park_plot_V3.py
+++ OSM/rail_and_park_plot_V3.py
@@ -11,13 +11,11 @@ import pyproj
 
 
 # # convert JSON to GeoJSON data 
-
 # ! osmtogeojson park_tokyo.json > park_tokyo.geojson
 # ! osmtogeojson rail_tokyo.json > rail_tokyo.geojson
 
 
-
-# load park anda rail geojson data 
+# load park and rail geojson data 
 rail_gpd = gpd.read_file('./rail_tokyo.geojson')
 park_gpd = gpd.read_file('./park_tokyo.geojson')
 
@@ -28,7 +26,6 @@ jp = gpd.read_file(file)
 
 
 # some parks are too far away from the center of tokyo, remove them
-
 # get the latitude of all parks 
 lat_list = []
 for i, row in park_gpd.iterrows():
@@ -38,17 +35,16 @@ for i, row in park_gpd.iterrows():
 park_gpd['centroid_lat'] = lat_list
 
 
-# remove the point geometry from the rail_gpd
+# remove the point geometry from rail_gpd
 no_point_rail_gpd = rail_gpd[rail_gpd['geometry'].geom_type != 'Point']
 # remove those lines without names 
 no_point_rail_gpd = no_point_rail_gpd[no_point_rail_gpd['name'].notnull()]
-
 # remove point geometry from park_gpd
 no_point_park_gpd = park_gpd[park_gpd['geometry'].geom_type != 'Point']
 
 
 
-# create park centroid coord gpd
+# create park centroid coord gpd for distance calculation later (park centroid and tack lines)
 park_centroid = no_point_park_gpd.to_crs({'init': 'epsg:3857'})
 
 # get the centroids of parks
@@ -58,12 +54,10 @@ for i, row in park_centroid.iterrows():
     centroid_list.append(geom.Point(centroid))
 
 park_centroid_gpd = gpd.GeoDataFrame(park_centroid, geometry = centroid_list)
-park_centroid_gpd.tail(3)
 
 
 
 clean_name = []
-
 for i in range(no_point_rail_gpd.shape[0]):
     name = no_point_rail_gpd['name'].iloc[i]
     string = name.split(' ')
@@ -71,9 +65,6 @@ for i in range(no_point_rail_gpd.shape[0]):
     clean_name.append(repalce_nanme)
 
 no_point_rail_gpd['clean_name'] = clean_name
-
-
-
 
 # keep only JR lines
 jr_line_name = ['山手線', '総武線', '青梅線', '中央線', '京浜東北線', '南武線', '埼京線', '中央本線', '常磐線', '五日市線', '湘南新宿ライン', 
@@ -84,16 +75,10 @@ jr_gpd = no_point_rail_gpd[no_point_rail_gpd['clean_name'].isin(jr_line_name)]
 
 
 
-
-
-# generate one color ofr each line
-
+# generate one color for each line
 random.Random(2019)
-
 num_color = len(np.unique(jr_gpd['clean_name']))
-
-color = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
-             for i in range(num_color)]
+color = ["#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)]) for i in range(num_color)]
 
 # create the color dict
 color_dict = {}
@@ -112,9 +97,7 @@ jr_gpd['color'] = color
 
 
 # find all parks that are within 200m to the train track being studied
-
 track_area_dict = {}
-
 track_name = np.unique(jr_gpd['clean_name'])
 points = park_centroid_gpd['geometry']
 
@@ -127,10 +110,13 @@ for name_idx in range(len(track_name)):
     sum_area = 0
     
     for i, line in enumerate(lines):
+        # find the shortest distance between the current line and all the parks
         all_park_to_line = [line.distance(point) for point in points]
         park_name = park_centroid_gpd['name'][np.array(all_park_to_line) < 200.]
         park_name_index = list(park_name.index)
 
+        # if the park is already within the 200m range with another line segment of the train track
+        # do not count it again
         for j in range(len(park_name_index)):
             if park_name_index[j] in(kept_park_index):
                 continue
@@ -142,14 +128,11 @@ for name_idx in range(len(track_name)):
     track_area_dict[track_name[name_idx]] = sum_area
 
 
-
+# find the train track with the largest area of parks 
 greenest_line = sorted(track_area_dict.items(), key=lambda item: item[1], reverse = True)[0][0]
-greenest_line
-
-
 
 # plot all JR line and the greenest line in red
-fig, ax = plt.subplots(figsize = (20,16)) 
+fig, ax = plt.subplots(figsize = (20, 16)) 
 
 tokyo_shp = jp[jp['KEN'] == '東京都']
 tokyo_shp.iloc[:53, :].plot(ax = ax, linewidth=0.25, edgecolor='black', color = 'white')
